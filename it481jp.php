5d1445f4239a6dc630803686246e808948b29380
JohnPellowKU <30849378+JohnPellowKU@users.noreply.github.com>
2017-08-15 16:04:12 -0500

    Add files via upload
    Unit 2 - Complete

--- it481jp.php
+++ it481jp.php
@@ -1,8 +1,25 @@
 <html>
 <head>
 	<title>IT481</title>
+	<style>
+	body {background-color: lightgray;}
+	table, th, td {
+    border: 1px solid black;
+}
+	h1 {font-style: italic;}
+	div {text-align: center;}
+	table {
+		margin-left: auto;
+		margin-right: auto;
+	}
+	</style>
 </head>
 <body>
+
+<div>
+<h1>Customer Data from Northwind</h1>
+<br>
+
 <?php
 	$conn = mysqli_connect("127.0.0.1", "root", "john8711", "northwind");
 
@@ -13,8 +30,14 @@
 		exit;
 	}
 
-	echo "Success: A proper connection to MySQL was made." . PHP_EOL;
-	echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
+	$sql = "SELECT CustomerID FROM customers";
+	
+	if ($result=mysqli_query($conn, $sql)) {
+		$rowcount = mysqli_num_rows($result);
+		printf("<h3><strong>Customer Count: %d </strong></h3><br><hr> \n", $rowcount);
+		
+		mysqli_free_result($result);
+	}
 	
 	echo "<br>";
 		
@@ -24,34 +47,23 @@
 	echo "<br>";	
 
 	if ($result->num_rows > 0) {
-	echo "Customer ID // Company Name // Contact Name // Contact Title // Address // City // Region // Postal Code // Country // Phone // Fax";
-	echo "<br>";
+	echo "<table> <tr> <th>Customer ID</th> <th>Company Name</th><th>Contact Name</th><th>Contact Title</th><th>Address</th><th>City</th><th>Region</th><th>Postal Code</th><th>Country</th><th>Phone</th><th>Fax</th></tr>";
 	
 	while($row = $result->fetch_assoc()) {
-		echo "<br>" . $row["CustomerID"]. " // " . $row["CompanyName"]. " // " . $row["ContactName"]
-					. " // " . $row["ContactTitle"]. " // " . $row["Address"]. " // " . $row["City"]
-					. " // " . $row["Region"]. " // " . $row["PostalCode"]. " // " . $row["Country"]
-					. " // " . $row["Phone"]. " // " . $row["Fax"];
+		echo "<tr> <td>" . $row["CustomerID"]. "</td><td>" . $row["CompanyName"]. "</td><td>" . $row["ContactName"]
+					. "</td><td>" . $row["ContactTitle"]. "</td><td>" . $row["Address"]. "</td><td>" . $row["City"]
+					. "</td><td>" . $row["Region"]. "</td><td>" . $row["PostalCode"]. "</td><td>" . $row["Country"]
+					. "</td><td>" . $row["Phone"]. "</td><td>" . $row["Fax"] . "</td></tr>";
 	}
 	} else {
 		echo "0 results";
 	}
-	
+	echo "</table>";
 	echo "<br>";
 	echo "<br>";
 	
-	$stmt = $conn->prepare("SELECT COUNT(CustomerID) FROM customers");
-
-	$stmt->execute();
-    $get_result =$stmt->get_result();
-
-    $row_count= $get_result->num_rows;
-
-    if($row_count>0)
-    {
-        print_r($get_result->fetch_assoc());
-    }
 	mysqli_close($conn);
 ?>
+</div>
 </body>
 </html>
